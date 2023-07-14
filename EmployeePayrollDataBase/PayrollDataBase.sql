60c8f91c1c0c564223b179ffca9fdd96cd2dfd91
Ritvik5 <sharma.ritvik56@gmail.com>
2023-07-14 20:04:41 +0530

    [Ritvik]Add: Creating Table

--- EmployeePayrollDataBase/PayrollDataBase.sql
+++ EmployeePayrollDataBase/PayrollDataBase.sql
@@ -3,3 +3,15 @@
 CREATE DATABASE Payroll_Service;
 
 USE Payroll_Service;
+
+--UC2(Creating Table/Structure)
+
+CREATE TABLE Employee_Payroll
+(
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	Name VARCHAR(20),
+	Salary MONEY,
+	StartDate Date
+);
+
+SELECT * FROM Employee_Payroll;
--- PayrollDataBase.sql
+++ PayrollDataBase.sql
@@ -3,3 +3,13 @@
 CREATE DATABASE Payroll_Service;
 
 USE Payroll_Service;
+
+CREATE TABLE Employee_Payroll
+(
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	Name VARCHAR(20),
+	Salary MONEY,
+	StartDate Date
+);
+
+SELECT * FROM Employee_Payroll;
