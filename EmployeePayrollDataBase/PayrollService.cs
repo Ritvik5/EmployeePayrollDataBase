d1ac400efe2c2c8f3db6e58db70daff4f195f7b5
Ritvik5 <sharma.ritvik56@gmail.com>
2023-07-14 22:42:52 +0530

    [Ritvik]Add: Creating Connection Using SqlConnection class

---
+++ EmployeePayrollDataBase/PayrollService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollDataBase
+{
+    public class PayrollService
+    {
+        public static SqlConnection con = new SqlConnection("data source = (localdb)\\MSSQLLocalDB; initial catalog = Payroll_Service; integrated security = true ");
+
+        public static void CheckConnection()
+        {
+            using (SqlConnection con1 = new SqlConnection("data source = (localdb)\\MSSQLLocalDB; initial catalog = Payroll_Service; integrated security = true "))
+            {
+                con.Open();
+                Console.WriteLine("Connection Established Successfully");
+            }
+        }
+    }
+}
