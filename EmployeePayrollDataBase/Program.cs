9da93fcad672dc3f618f493ebce2c55299d58466
Ritvik5 <sharma.ritvik56@gmail.com>
2023-07-14 19:41:46 +0530

    [Ritvik]Add: Welcome Message

---
+++ EmployeePayrollDataBase/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeePayrollDataBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll DataBase.");
+        }
+    }
+}
