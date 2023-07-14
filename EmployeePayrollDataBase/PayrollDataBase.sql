da9823bb1a0233fd4aec84de0d37cb80d97ab699
Ritvik5 <sharma.ritvik56@gmail.com>
2023-07-14 20:43:40 +0530

    [Ritvik]Add: Adding Gender Column

--- EmployeePayrollDataBase/PayrollDataBase.sql
+++ EmployeePayrollDataBase/PayrollDataBase.sql
@@ -35,3 +35,18 @@ WHERE Name = 'Jack';
 
 SELECT * FROM Employee_Payroll
 WHERE StartDate between '2019-01-01' and getdate();
+
+--UC6
+INSERT INTO Employee_Payroll(Name,Salary,StartDate) 
+VALUES('Julie',25000,'2019-01-02'),
+	  ('Monica',22000,'2015-01-03');
+
+
+ALTER TABLE Employee_Payroll ADD Gender CHAR(1);
+
+UPDATE Employee_Payroll SET Gender = 'M'
+WHERE Name = 'Jack' OR Name = 'Smith' OR Name = 'John' OR Name = 'Snow';
+
+UPDATE Employee_Payroll SET Gender = 'F'
+WHERE Name = 'Julie' OR NAME = 'Monica';
+
